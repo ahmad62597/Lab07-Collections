e6a45ed7268d45030b27b79a03fe879adb79b29e
ahmad <ahmad@DESKTOP-7Q4GTCL>
2019-01-16 20:06:04 -0800

    enum

--- Classes/Deck.cs
+++ Classes/Deck.cs
@@ -1,8 +1,25 @@
 ﻿using System;
 
-public class Class1
+namespace CardDeck.Classes
 {
-	public Class1()
-	{
-	}
-}
+    public class Deck<T> : IEnumerable
+    {
+        T[] cards = new T[10];
+        public int count = 0;
+
+
+        // Enumerator to allow foreach for cards
+        // returns each item from the deck
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < count; i++)
+            {
+                yield return cards[i];
+            }
+        }
+
+        // Required to allow enumeration 
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
