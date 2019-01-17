e77bbe056e59619f21b7a815432b733509a6b3c8
ahmad <ahmad@DESKTOP-7Q4GTCL>
2019-01-16 19:52:54 -0800

    card class

---
+++ Classes/Card.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CardDeckCollections.Classes
+{
+    public class Card
+    {
+        public Suit Suit { get; set; }
+        public string Type { get; set; }
+
+        public Card(Suit suit, string type)
+        {
+            Suit = suit;
+            Type = type;
+        }
+    }
+
+}
+
